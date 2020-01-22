b1cf8d0fcb43bd2c2ae0a054625e40fe192e2263
Holger Will <h.will@klimapartner.de>
2020-01-22 06:28:00 +0100

    refactor init function

--- functions/colors.js
+++ functions/colors.js
@@ -1,26 +1,28 @@
 const colorNames = require('./color-names.json')
 module.exports = function color (...args) {
-  let bg, r, g, b, s, col
+  let bg, rgb, s, col
   if (typeof args[0] === 'string') {
     col = args[0].substr(0, 1) === '#' ? args[0] : colorNames[args[0]]
     bg = args[1] || 0
     s = args[2] || 10
-    {r, g, b} = ...getColorFromRGB(col)
+    rgb = getColorFromRGB(col)
   } else {
-    r = args[0]
-    g = args[1]
-    b = args[2]
+    rgb = {
+      r: args[0],
+      g: args[1],
+      b: args[2]
+    }
     bg = args[3] || 0
     s = args[4] || 10
   }
   const l = bg ? 48 : 38
-  return `\x1b[${l};2;${r};${g};${b}m\x1b[${s}m`
+  return `\x1b[${l};2;${rgb.r};${rgb.g};${rgb.b}m\x1b[${s}m`
 }
 
 function getColorFromRGB (hexCode) {
   return {
-    r: parseInt(col.substr(1, 2), 16),
-    g: parseInt(col.substr(3, 2), 16),
-    b: parseInt(col.substr(5, 2), 16)
+    r: parseInt(hexCode.substr(1, 2), 16),
+    g: parseInt(hexCode.substr(3, 2), 16),
+    b: parseInt(hexCode.substr(5, 2), 16)
   }
 }
--- functions/init.js
+++ functions/init.js
@@ -1,17 +1,37 @@
 const fs = require('fs')
 const color = require('./colors.js')
+const COL1 = color('limegreen')
+const COL2 = color('limegreen')
+const COL3 = color('royalblue')
+const COL4 = color('crimson')
+const RESET = '\x1b[0m'
+
 module.exports = function init () {
-  const COL1 = color('royalblue')
-  const COL2 = color('limegreen')
-  const COL3 = color('indigo')
-  const COL4 = color('crimson')
-  const RESET = '\x1b[0m'
   const wd = process.cwd().split('/')
   const name = wd[wd.length - 1]
   const baseName = 'template-kaskadi-element'
-  const baseClassName = 'TemplateKaskadiElement'
-  const baseNameRegex = new RegExp(baseName, 'g')
-  const baseClassNameRegex = new RegExp(baseClassName, 'g')
+  updatePackageJSON(name)
+  replaceNameInFile(process.cwd() + '/test/basic.test.js', baseName, name)
+  replaceNameInFile(process.cwd() + '/README.md', baseName, name)
+  replaceNameInFile(process.cwd() + '/example/index.html', baseName, name)
+  replaceNameInFile(`${process.cwd()}/${baseName}.js`, baseName, name)
+  replaceNameInFile(`${process.cwd()}/${baseName}.js`, snakeToCamel(baseName), snakeToCamel(name))
+  fs.renameSync(`${baseName}.js`, name + '.js')
+  log(COL1, 'rename', `${baseName}.js to ${COL3}${name}.js`, true)
+}
+
+function replaceNameInFile (fileName, oldName, newName) {
+  const oldNameRegex = new RegExp(oldName, 'g')
+  if (fs.existsSync(fileName)) {
+    const file = fs.readFileSync(fileName, 'utf8')
+    fs.writeFileSync(fileName, file.replace(oldNameRegex, newName), 'utf8')
+    updateNotice(fileName)
+  } else {
+    log(COL4, 'error', `${fileName} not found`, false)
+  }
+}
+
+function updatePackageJSON (name) {
   const f = fs.readFileSync(process.cwd() + '/package.json', 'utf8')
   const p = JSON.parse(f)
   p.name = '@kaskadi/' + name
@@ -22,34 +42,16 @@ module.exports = function init () {
   p.kaskadi['s3-push'].files[0].src = `${name}.js`
   p.kaskadi['s3-push'].files[0].dest = `modules/@kaskadi/${name}/{branch}${name}.js`
   p.kaskadi['s3-push'].files[1].dest = `modules/@kaskadi/${name}/{branch}example/index.html`
-
   fs.writeFileSync(process.cwd() + '/package.json', JSON.stringify(p, null, 2), 'utf8')
-  console.log(`${COL1}updated ${RESET} package.json ${COL2}✓${RESET} `)
-  const f2 = fs.readFileSync(process.cwd() + '/test/basic.test.js', 'utf8')
+  updateNotice('package.json')
+}
 
-  fs.writeFileSync(process.cwd() + '/test/basic.test.js', f2.replace(baseNameRegex, name), 'utf8')
-  console.log(`${COL1}updated ${RESET} test/basic.test.js ${COL2}✓${RESET} `)
+function updateNotice (name) {
+  log(COL1, 'updated', name, true)
+}
 
-  const f2a = fs.readFileSync(process.cwd() + '/README.md', 'utf8')
-  fs.writeFileSync(process.cwd() + '/README.md', f2a.replace(baseNameRegex, name), 'utf8')
-  console.log(`${COL1}updated ${RESET} README.md ${COL2}✓${RESET} `)
-
-  const f3 = fs.readFileSync(process.cwd() + '/example/index.html', 'utf8')
-  fs.writeFileSync(process.cwd() + '/example/index.html', f3.replace(baseNameRegex, name), 'utf8')
-  console.log(`${COL1}updated ${RESET} example/index.html ${COL2}✓${RESET} `)
-  try {
-    let f4 = fs.readFileSync(process.cwd() + `/${baseName}.js`, 'utf8')
-    f4 = f4.replace(baseNameRegex, name)
-    f4 = f4.replace(baseClassNameRegex, snakeToCamel(name))
-    fs.writeFileSync(process.cwd() + `/${baseName}.js`, f4, 'utf8')
-    console.log(`${COL1}updated ${RESET} ${baseName}.js ${COL2}✓${RESET} `)
-    fs.renameSync(`${baseName}.js`, name + '.js')
-    console.log(`${COL1}renamed ${RESET} ${baseName}.js to ${COL3}${name}.js ${COL2}✓${RESET} `)
-  } catch (err) {
-    if (err) {
-      console.log(`${COL4}error ${RESET} ${baseName}.js not found ${COL4}x${RESET} `)
-    }
-  }
+function log (col, type, msg, check) {
+  console.log(`${col}${type} ${RESET} ${msg} ${check ? COL2 : COL4}${check ? '✓' : 'x'}${RESET} `)
 }
 
 function snakeToCamel (word) {
