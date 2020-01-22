93f857598ffc5b960048a12389ed27e7db658efd
Holger Will <h.will@klimapartner.de>
2020-01-22 06:44:29 +0100

    cleaner cli color function

--- functions/colors.js
+++ functions/colors.js
@@ -1,22 +1,41 @@
 const colorNames = require('./color-names.json')
 module.exports = function color (...args) {
-  let bg, rgb, s, col
+  let rgb
   if (typeof args[0] === 'string') {
-    col = args[0].substr(0, 1) === '#' ? args[0] : colorNames[args[0]]
-    bg = args[1] || 0
-    s = args[2] || 10
-    rgb = getColorFromRGB(col)
+    rgb = extractFromString(args[0])
+    setMember(rgb, 'bg', args, 1)
+    setMember(rgb, 'style', args, 2)
   } else {
     rgb = {
       r: args[0],
       g: args[1],
       b: args[2]
     }
-    bg = args[3] || 0
-    s = args[4] || 10
+    setMember(rgb, 'bg', args, 3)
+    setMember(rgb, 'style', args, 4)
   }
+  return cliColorString(rgb)
+}
+
+function extractFromString (str) {
+  const firstLetter = args[0].substr(0, 1)
+  if (firstLetter === '#') {
+    return getColorFromRGB(str)
+  } else {
+    return getColorFromRGB(colorNames[str])
+  }
+}
+
+function cliColorString (options) {
+  const opt = {...{bg: 0, style: 10}, ...options}
   const l = bg ? 48 : 38
   return `\x1b[${l};2;${rgb.r};${rgb.g};${rgb.b}m\x1b[${s}m`
+}
+
+function setMember (obj, name, arr, offs) {
+  if (arr.length < offs) {
+    obj[name] = arr[offs]
+  }
 }
 
 function getColorFromRGB (hexCode) {
