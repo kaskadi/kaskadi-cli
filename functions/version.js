4fadd0f9214e87e219d888ed8846f867b7dd5738
Holger Will <h.will@klimapartner.de>
2020-01-26 08:09:30 +0100

    rename var

--- functions/version.js
+++ functions/version.js
@@ -1,20 +1,20 @@
 const exec = require('child_process').exec
-const p = require(process.cwd() + "/package.json")
+const p = require(process.cwd() + '/package.json')
 async function version () {
-  let version = 0 
-  try{
+  let version = 0
+  try {
     version = (await getNPMVersion(p.name)).trim()
-  }catch(err){
-    console.log("package not on npm! aborting")
+  } catch (err) {
+    console.log('package not on npm! aborting')
   }
-  if(p.version === version){
-    console.log(JSON.stringify({package: p.version, npm: version}, null, 2))
+  if (p.version === version) {
+    console.log(JSON.stringify({ package: p.version, npm: version }, null, 2))
   }
 }
 
-function getNPMVersion (package) {
+function getNPMVersion (name) {
   return new Promise((resolve, reject) => {
-    exec(`npm view ${package} version`, (err, stdout, stderr) => {
+    exec(`npm view ${name} version`, (err, stdout, stderr) => {
       if (err) {
         reject(err)
       } else {
