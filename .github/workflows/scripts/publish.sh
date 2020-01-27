5b29b7e6c61c195734093fd38312ff42cc1a2518
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-27 18:47:43 +0100

    Testing fix for registry not found error on publish

--- .github/workflows/scripts/publish.sh
+++ .github/workflows/scripts/publish.sh
@@ -3,10 +3,10 @@ getVersion() {
   node -e "console.log(require('./package.json').version)"
 }
 # create .npmrc file for authenticating user
-touch ~/.npmrc
+touch .npmrc
 echo "_auth=\"$NPM_TOKEN\"
 email=a.lemaire@klimapartner.de
-always-auth=true" > ~/.npmrc
+always-auth=true" > .npmrc
 # compare current and remote version
 CURRENT_VERSION="$(getVersion)"
 REMOTE_VERSION="$(npm view kaskadi-cli version)"
@@ -20,4 +20,4 @@ git config --global user.email 'a.lemaire@klimapartner.de'
 git commit -am "Upgraded to $CURRENT_VERSION"
 git push
 npm publish
-rm ~/.npmrc
+rm .npmrc
