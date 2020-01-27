40e3e7fabc6c5cf1f2602aab2d4ed38d330adf83
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-27 18:09:04 +0100

    Testing action with new publish script

--- .github/workflows/scripts/publish.sh
+++ .github/workflows/scripts/publish.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+getVersion() {
+  node -e "console.log(require('./package.json').version)"
+}
 # create .npmrc file for authenticating user
 touch ~/.npmrc
 echo "_auth=\"$NPM_TOKEN\"
 email=a.lemaire@klimapartner.de
 always-auth=true" > ~/.npmrc
 # compare current and remote version
-CURRENT_VERSION="$(npm run version)"
+CURRENT_VERSION="$(getVersion)"
 REMOTE_VERSION="$(npm view kaskadi-cli version)"
 if [ "$CURRENT_VERSION" == "$REMOTE_VERSION" ]
 then
   npm --no-git-tag-version version patch
-  CURRENT_VERSION="$(npm run version)"
+  CURRENT_VERSION="$(getVersion)"
 fi
 git config --global user.name 'Alexis Lemaire'
 git config --global user.email 'a.lemaire@klimapartner.de'
