e22b61451fddb4581f1e749186911e1e0b8da15c
Alexis <a.lemaire@klimapartner.de>
2020-02-06 16:58:59 +0000

    Upgraded to 0.0.27

--- functions/init/lambda-init.js
+++ functions/init/lambda-init.js
@@ -6,7 +6,7 @@ module.exports = function lambdaInit (wd, baseName, name) {
   const fileData = [{
     baseName,
     name,
-    paths: ['README.md', 'serverless.yml', ]
+    paths: ['README.md', 'serverless.yml']
   }]
   const initData = { jsonData, fileData }
   initFiles(wd, initData)
