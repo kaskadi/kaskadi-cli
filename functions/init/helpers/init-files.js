3b481ea4cfd48861d6ef48fb88c5e0838225522f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-06 16:36:49 +0100

    Fixed old syntax mismatch

--- functions/init/helpers/init-files.js
+++ functions/init/helpers/init-files.js
@@ -3,7 +3,7 @@ const updatePackageJSON = require('./update-package-json.js')
 
 module.exports = function initFiles (wd, initData) {
   const { jsonData, fileData } = initData
-  updatePackageJSON(wd, json.baseName, json.name)
+  updatePackageJSON(wd, jsonData.baseName, jsonData.name)
   // initData is an array of objects containing information for the init operation on files
   fileData.forEach(data => {
     data.paths.forEach(path => {
