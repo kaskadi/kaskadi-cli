f4cd9061d5931be9a6561bcdd42c1767bac2c18c
Holger Will <h.will@klimapartner.de>
2020-02-07 08:34:04 +0100

    remove old comment

--- kaskadi.js
+++ kaskadi.js
@@ -1,18 +1,5 @@
 #!/usr/bin/env node
 
-// const readline = require('readline');
-// function askQuestion(query) {
-//     const rl = readline.createInterface({
-//         input: process.stdin,
-//         output: process.stdout,
-//     });
-//
-//     return new Promise(resolve => rl.question(query, ans => {
-//         rl.close();
-//         resolve(ans);
-//     }))
-// }
-
 // TODO: add init for each type of template and extend function collection here
 
 const functions = {
