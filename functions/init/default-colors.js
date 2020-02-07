6629137d9eb7c3f0bfc5c49eac7dd07a91afc75c
Holger Will <h.will@klimapartner.de>
2020-02-07 08:23:01 +0100

    use @kaskadi/better-colors

--- functions/init/default-colors.js
+++ functions/init/default-colors.js
@@ -1,5 +1,5 @@
-const color = require('../colors/colors.js')
+const color = require('@kaskadi/better-colors')
 exports.COL1 = color('royalblue')
 exports.COL2 = color('limegreen')
 exports.COL4 = color('crimson')
-exports.RESET = '\x1b[0m'
+exports.RESET = color.RESET
