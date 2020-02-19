78faca71d00eb04c84be7cca5a262796ea6e8db4
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-19 14:53:51 +0100

    Fixed log call position to only log error or success (not both)

--- functions/init/helpers/rename-files.js
+++ functions/init/helpers/rename-files.js
@@ -6,9 +6,9 @@ module.exports = function renameFiles (wd, files) {
   files.forEach(file => {
     if (fs.existsSync(`${wd}/${file.baseName}`)) {
       fs.renameSync(`${wd}/${file.baseName}`, `${wd}/${file.name}`)
+      log(defaultColors.COL1, 'rename', `${file.baseName} to ${defaultColors.COL1}${file.name}`, true)
     } else {
       log(defaultColors.COL4, 'error', `${file.baseName} not found`, false)
     }
-    log(defaultColors.COL1, 'rename', `${file.baseName} to ${defaultColors.COL1}${file.name}`, true)
   })
 }
