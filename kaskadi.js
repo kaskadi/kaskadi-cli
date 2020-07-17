96d5a6c03a5330bd213bcb2aaa0e50af59da47c3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-17 17:51:32 +0200

    Hotfix: removed comment

--- kaskadi.js
+++ kaskadi.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-// TODO: add init for each type of template and extend function collection here
-
 const functions = {
   init: require('./functions/init/init.js'),
   render: require('./functions/render.js'),
