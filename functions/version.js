e486bedc5a69e7fff1c00dacff2fed084e1bc3cd
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-21 13:37:58 +0200

    Updated version CLI function

--- functions/version.js
+++ functions/version.js
@@ -1,27 +1,7 @@
-const exec = require('child_process').exec
-const p = require(process.cwd() + '/package.json')
-async function version () {
-  let version = 0
-  try {
-    version = (await getNPMVersion(p.name)).trim()
-  } catch (err) {
-    console.log('package not on npm! aborting')
-  }
-  if (p.version === version) {
-    console.log(JSON.stringify({ package: p.version, npm: version }, null, 2))
-  }
+module.exports = () => {
+  const { spawnSync } = require('child_process')
+  const path = require('path')
+  const p = require(path.join(__dirname, '..', 'package.json'))
+  const version = spawnSync('npm', ['view', p.name, 'version']).stdout.toString().trim()
+  console.log(JSON.stringify({ local_installation: p.version, npm: version }, null, 2))
 }
-
-function getNPMVersion (name) {
-  return new Promise((resolve, reject) => {
-    exec(`npm view ${name} version`, (err, stdout, stderr) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(stdout)
-      }
-    })
-  })
-}
-
-module.exports = version
