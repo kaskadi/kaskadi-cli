d223fc523205fb0c7f175f76cb33c2aff28a631d
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-27 18:35:20 +0200

    Implemented project monitoring logic

--- functions/snyk/monitor/monitor.js
+++ functions/snyk/monitor/monitor.js
@@ -1,13 +1,35 @@
-// const promptToken = require('./helpers/prompt-token.js')
+const checkEnv = require('./helpers/check-env.js')
+const setEnv = require('./helpers/set-env.js')
 const getRepoInfo = require('./helpers/get-repo-info.js')
+const monitorRepo = require('./helpers/monitor-repo.js')
+
+const envVarsData = [{
+  name: 'SNYK_API_TOKEN',
+  promptMessage: 'Please input your Snyk API token',
+  validateHandler: input => !input ? 'Please provide your Snyk API token' : true
+},
+{
+  name: 'SNYK_ORG_ID',
+  promptMessage: 'Please input your Snyk organization ID',
+  validateHandler: input => !input ? 'Please provide your Snyk organization ID' : true
+},
+{
+  name: 'SNYK_INTEGRATION_ID',
+  promptMessage: 'Please input your Snyk integration ID',
+  validateHandler: input => !input ? 'Please provide your Snyk integration ID' : true
+}]
 
 module.exports = async () => {
-  if (process.env.GITHUB_ACTIONS && !process.env.SNYK_TOKEN) {
-    console.log('You have to provide your Snyk API token as a SNYK_TOKEN environment variable when running inside of a GitHub Action...')
-    process.exit(1)
+  if (process.env.GITHUB_ACTIONS) {
+    checkEnv(envVarsData)
+  } else {
+    await setEnv(envVarsData)
   }
-  await getRepoInfo().then(console.log)
-  // if repo public, we will monitor as a public project. If it's not (or it doesn't exist), then we monitor as a local/private project
-  // process.env.SNYK_TOKEN = process.env.SNYK_TOKEN || await promptToken()
-  // spawnSync('node', [snykCli, 'monitor'], { stdio: 'inherit' })
+  await getRepoInfo()
+    .then(monitorRepo)
+    .catch(err => {
+      console.log('Something happened while trying to monitor your project...')
+      console.log(err)
+      process.exit(1)
+    })
 }
