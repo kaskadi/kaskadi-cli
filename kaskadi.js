e9a539a9053ff7c74cacfdf6bcad7d2730c785f7
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-27 11:49:18 +0200

    Using commander to define CLI

--- kaskadi.js
+++ kaskadi.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
-const functions = {
-  init: require('./functions/init/init.js'),
-  render: require('./functions/render.js'),
-  version: require('./functions/version.js')
-}
+const { Command } = require('commander')
+const program = new Command()
+program
+  .name('kaskadi')
+  .usage('[options] command')
+  .version(require('./package.json').version)
+program
+  .command('init <type>')
+  .description('initialize a repository. <type> argument defines which kind of template the repository is following. Valid values are: action, api, lambda, element and layer.')
+  .action(require('./functions/init/init.js'))
+program
+  .command('render')
+  .option('-e, --element <element>', 'element to render')
+  .description('render the given element')
+  .action(require('./functions/render.js'))
 
-functions[process.argv[2]](process.argv.splice(3))
+program.parse(process.argv)
