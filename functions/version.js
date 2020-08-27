29fb2b0d120cdc98c1a39a549b69d9256ac373f7
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-27 11:59:31 +0200

    Removed version handler (integrated in commander)

--- functions/version.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-  const { spawnSync } = require('child_process')
-  const path = require('path')
-  const p = require(path.join(__dirname, '..', 'package.json'))
-  const version = spawnSync('npm', ['view', p.name, 'version']).stdout.toString().trim()
-  console.log(JSON.stringify({ local_installation: p.version, npm: version }, null, 2))
-}
