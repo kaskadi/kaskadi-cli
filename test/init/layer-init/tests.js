3e31db43e9e181bdc5de620032126be73ee67331
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-14 13:22:50 +0200

    Updated tests to include package-lock.json into init

--- test/init/layer-init/tests.js
+++ test/init/layer-init/tests.js
@@ -22,6 +22,12 @@ module.exports = (root, baseName, baseCamelName, folderName, camelName) => {
     const test = deepEqual(pjson, pjsonValid)
     test.should.equal(true)
   })
+  it(`should rename all occurences of ${baseName} to ${folderName} in package-lock.json`, () => {
+    const pjson = JSON.parse(fs.readFileSync(`${root}working-data/package-lock.json`, 'utf8'))
+    const pjsonValid = JSON.parse(fs.readFileSync(`${root}validation/package-lock.json`, 'utf8'))
+    const test = deepEqual(pjson, pjsonValid)
+    test.should.equal(true)
+  })
   it('should not throw on second run', () => {
     layerInit.should.not.throw(`${root}working-data`, baseName, folderName)
   })
