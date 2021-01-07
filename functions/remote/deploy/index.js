7f339cefb6cd55abeaf0e227e54f888bbc0f5f50
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-07 14:48:21 +0100

    Removed --dry-run flag

--- functions/remote/deploy/index.js
+++ functions/remote/deploy/index.js
@@ -2,5 +2,5 @@ const { spawnSync } = require('child_process')
 
 module.exports = ({ remote, branch, sshPath, port }) => {
   process.env.GIT_SSH_COMMAND = `ssh -i ${sshPath} -p ${port} -o IdentitiesOnly=yes`
-  spawnSync('git', ['push', '--dry-run', remote, branch], { stdio: 'inherit' })
+  spawnSync('git', ['push', remote, branch], { stdio: 'inherit' })
 }
