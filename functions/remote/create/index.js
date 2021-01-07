c7527d0514c46eb86d986635633dd6cbe794187c
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-07 17:16:28 +0100

    Implemented logic for remote repo creation module

--- functions/remote/create/index.js
+++ functions/remote/create/index.js
@@ -1,1 +1,25 @@
-module.exports = console.log
+function getWriteHookCmds (content) {
+  return content.map(line => `echo "${line}" >> hooks/post-receive`)
+}
+
+module.exports = async ({ ip, sshPath, port }) => {
+  const ssh = require('../utils/ssh.js')(sshPath, port, ip)
+  const { bare, working } = require('../utils/get-remote-paths.js')()
+  const commands = [
+    `mkdir -p ${bare}`,
+    `mkdir -p ${working}`,
+    `cd ${bare}`,
+    'git init --bare',
+    'touch hooks/post-receive',
+    ...getWriteHookCmds([
+      '#!/bin/sh',
+      `GIT_WORK_TREE=${working} git checkout -f`,
+      'PWD="$(pwd)"',
+      `cd ${working}`,
+      'npm i &>/dev/null',
+      'cd $PWD'
+    ]),
+    'chmod +x hooks/post-receive'
+  ]
+  ssh(commands.join(' && '))
+}
