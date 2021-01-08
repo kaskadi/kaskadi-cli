1ef3cccb60aee3727f498c0c19c89312ac6be731
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-08 11:40:11 +0100

    Added chown for group update to standard hook

--- functions/remote/create/hooks-data/standard.sh
+++ functions/remote/create/hooks-data/standard.sh
@@ -10,4 +10,14 @@ if [ -f {{working-copy}}/package.json ]
       npm run build &>/dev/null
   fi
 fi
+PUBLIC_DIR="{{working-copy}}/public"
+BUILD_DIR="{{working-copy}}/build"
+DIRS=( $PUBLIC_DIR $BUILD_DIR )
+for dir in "${DIRS[@]}"
+do
+  if [ -d $dir ]
+    then
+      chown -R git:www-data $dir
+  fi
+done
 cd $PWD
