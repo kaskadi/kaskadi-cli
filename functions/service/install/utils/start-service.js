3d42775203202e75a86e7b3a2d3763777bbe4d77
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-29 14:49:40 +0100

    Added logic for starting service

--- functions/service/install/utils/start-service.js
+++ functions/service/install/utils/start-service.js
@@ -1,28 +1,30 @@
 const execCmd = require('./exec-cmd.js')
 
 module.exports = (opts) => {
+  console.log('INFO: starting service...')
   reload(opts)
   start(opts)
   if (opts.reboot) {
+    console.log('INFO: enabling service restart on machine reboot...')
     enable(opts)
   }
 }
 
-function getSystemCtlCmd (isSystem, cmd) {
-  return `${!isSystem ? '--user ' : ''}${cmd}`
+function getSystemCtlCmds (isSystem, ...cmds) {
+  return [...isSystem ? [] : ['--user'], ...cmds]
 }
 
 function reload (opts) {
   const { system } = opts
-  execCmd(system, 'systemctl', getSystemCtlCmd(system, 'daemon-reload'))
+  execCmd(system, 'systemctl', ...getSystemCtlCmds(system, 'daemon-reload'))
 }
 
 function start (opts) {
   const { system, name } = opts
-  execCmd(system, 'systemctl', getSystemCtlCmd(system, 'start'), name)
+  execCmd(system, 'systemctl', ...getSystemCtlCmds(system, 'start', name))
 }
 
 function enable (opts) {
   const { system, name } = opts
-  execCmd(system, 'systemctl', getSystemCtlCmd(system, 'enable'), name)
+  execCmd(system, 'systemctl', ...getSystemCtlCmds(system, 'enable', name))
 }
