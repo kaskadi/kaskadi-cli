cc707ad219dddc3cafb0deff30aa94d8c616887b
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-29 17:51:08 +0100

    Updated uninstall handler

---
+++ functions/service/uninstall/index.js
@@ -0,0 +1,11 @@
+const checkOpts = require('./utils/check-opts.js')
+const servicesLookup = require('./utils/services-lookup.js')
+const uninstallServices = require('./utils/uninstall-services.js')
+
+module.exports = (opts) => {
+  if (!checkOpts(opts)) {
+    console.log('ERROR: no name was provided for the service to uninstall... Please provide a service name via the option -n/--name.')
+    process.exit(1)
+  }
+  uninstallServices(servicesLookup(opts))
+}
