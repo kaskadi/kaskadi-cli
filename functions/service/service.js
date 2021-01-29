9196adc9029ae84108f60adcb4f1aeafa540599a
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-29 13:05:29 +0100

    Added service module entry point

---
+++ functions/service/service.js
@@ -0,0 +1,9 @@
+const validActions = ['install', 'delete']
+
+module.exports = function (action, opts) {
+  if (!validActions.includes(action)) {
+    console.log(`ERROR: ${action} is not a valid operation for the service module...`)
+  } else {
+    require(`./${action}/index.js`)(opts)
+  }
+}
